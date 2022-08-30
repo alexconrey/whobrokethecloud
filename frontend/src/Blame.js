c10e7bab4f8672e9dbfce1335814a4894d4ee458
Alex Conrey <alex.conrey@wellspring.com>
2022-08-30 15:07:21 -0500

    working frontend

---
+++ frontend/src/Blame.js
@@ -0,0 +1,105 @@
+import { useState, useEffect } from 'react';
+
+// eslint-disable-next-line
+Object.defineProperty(String.prototype, 'capitalize', {
+    value: function() {
+      return this.charAt(0).toUpperCase() + this.slice(1);
+    },
+    enumerable: false
+  });
+
+function RenderOutageRow(props) {
+    return (
+        <tr>
+            <td>{props.provider.capitalize()}</td>
+            <td>{props.service}</td>
+            <td>{props.starttime}</td>
+        </tr>
+    )
+}
+function RenderOutageTable(props) {
+    var outages = [];
+    props.outages.map((outage) => {
+        outage.map((test) => {
+            outages.push(
+                <RenderOutageRow provider={test.Provider} service={test.Service} starttime={test.StartTime} />
+            )
+        })
+    })
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Provider</th>
+                    <th>Service</th>
+                    <th>Start Time</th>
+                </tr>
+            </thead>
+            <tbody>
+                {outages}
+            </tbody>
+        </table>
+
+    )
+}
+
+export default function Blame() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    var outageDict = {};
+    var currentOutageCount = {};
+    var outageArr = [];
+
+    useEffect(() => {
+        fetch("http://localhost/api/outages")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                setIsLoaded(true);
+                setItems(result);
+            },
+            (error) => {
+                setIsLoaded(true);
+                setError(error);
+            }
+        )
+    }, [])
+
+    if (error) {
+        return <div>Error: {error.message}</div>
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        for (const [key, value] of Object.entries(items)) {
+            if (! Array.isArray(outageDict[key])) {
+                outageDict[key] = [];
+            }
+            outageDict[key].push(value)
+            outageArr.push(value)
+        }
+
+        for (const [key, value] of Object.entries(outageDict)) {
+            currentOutageCount[key] = value.length
+        }
+
+        const affectedProviders = Object.keys(currentOutageCount).length;
+
+        if (affectedProviders === 0) {
+            return <div>No outages, surprisingly!</div>
+        // } else if (affectedProviders == 1) {
+        //     return <h1>We're blaming {Object.keys(currentOutageCount)[0].capitalize()}.</h1>
+        } else {
+            return (
+                <div>
+                    <h1>Well, the internet is on fire.</h1>
+                    <div>
+                        <div>Current Outages</div>
+                        <RenderOutageTable outages={outageArr} />
+                    </div> 
+                </div>
+            )
+        }
+    }
+}
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,91 +1,59 @@
 // import { useEffect } from "react/cjs/react.production.min";
 import { useState, useEffect } from 'react';
+import Blame from './Blame';
 
 function Home() {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [items, setItems] = useState([]);
+    return (
+        <Blame />
+    )
+    // const [error, setError] = useState(null);
+    // const [isLoaded, setIsLoaded] = useState(false);
+    // const [items, setItems] = useState([]);
 
-    var outageDict = new Object();
-    var currentOutages = new Object();
+    // var outageDict = new Object();
+    // var currentOutageCount = new Object();
 
-    useEffect(() => {
-        fetch("http://localhost/api/outages")
-        .then(res => res.json())
-        .then(
-            (result) => {
-                setIsLoaded(true);
-                setItems(result);
-            },
-            (error) => {
-                setIsLoaded(true);
-                setError(error);
-            }
-        )
-    }, [])
+    // useEffect(() => {
+    //     fetch("http://localhost/api/outages")
+    //     .then(res => res.json())
+    //     .then(
+    //         (result) => {
+    //             setIsLoaded(true);
+    //             setItems(result);
+    //         },
+    //         (error) => {
+    //             setIsLoaded(true);
+    //             setError(error);
+    //         }
+    //     )
+    // }, [])
 
-    if (error) {
-        return <div>Error: {error.message}</div>
-    } else if (!isLoaded) {
-        return <div>Loading...</div>;
-    } else {
-        for (const [key, value] of Object.entries(items)) {
-            outageDict[key] = value
-        }
+    // if (error) {
+    //     return <div>Error: {error.message}</div>
+    // } else if (!isLoaded) {
+    //     return <div>Loading...</div>;
+    // } else {
+    //     for (const [key, value] of Object.entries(items)) {
+    //         outageDict[key] = value
+    //     }
 
-        for (const [key, value] of Object.entries(outageDict)) {
-            currentOutages[key] = value.length
-        }
+    //     for (const [key, value] of Object.entries(outageDict)) {
+    //         currentOutageCount[key] = value.length
+    //     }
 
-        return (
-            Object.entries(currentOutages)
-                .map(([key, val]) => {
-                    return (
-                        <>
-                            <li>{key}</li>
-                            <li>{val}</li>
-                        </>
-                    )
-                    console.log(val)
-            })
-        )
-
-        // return (
-        //     // {Object.entries(outageDict)
-        //     //     .map(([key, val]) => {
-        //     //         console.log(val)
-        //     //         return (
-        //     //             <ul id={key}>
-        //     //                 {val.map(element => {
-        //     //                     return <li>{element.Service}</li> 
-        //     //                 })}
-        //     //             </ul>
-        //     //         )              
-        //     //     })}
-        //     Object.entries(outageDict)
-        //     .map(([key, val]) => {
-        //         console.log(val)
-        //         return (
-        //             <ul id={key}>
-        //                 {Object.entries(val)
-        //                 .map(([k, v])) => {
-        //                     return ( <li>{v}</li> )
-        //                 }}
-        //                 <li id={key}>{val.Service}</li>
-        //             </ul>
-        //         )              
-        //     })
-        // )
-        // console.log(outageDict)
-        // console.log(items)
-        // return (
-        //     <ul>
-        //         {items.map((element, idx) => (
-        //             {element}
-        //         ))}
-        //     </ul>
-        // );
-    }
+    //     return (
+    //         Object.entries(currentOutageCount)
+    //             .map(([key, val]) => {
+    //                 return (
+    //                     <>
+    //                         <li>{key}</li>
+    //                         <li>{val}</li>
+    //                     </>
+    //                 )
+    //                 console.log(val)
+    //         })
+    //     )
+    // }
 }
 
 export default Home;
